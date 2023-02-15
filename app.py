69932817265d578aea5dd1c428e3f664de41ace9
truonghoangduy <truonghoangduy03111999@gmail.com>
2023-02-15 15:34:31 +0700

    update dummy

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_restful import Api, Resource
 
 from services.dummy_service import DummyService
+from dotenv import load_dotenv
+
+load_dotenv("./module_editly.env")
 
 app = Flask(__name__)
 api = Api(app)
 
 
-api.add_resource(DummyService, "/dummy")
+api.add_resource(DummyService, "/dummy/<vin>")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- services/dummy_service.py
+++ services/dummy_service.py
@@ -1,10 +1,27 @@
 from flask_restful import Resource
 
 from models.dummy_model import DummyModel
+from modules.editly_template import *
+from modules.editly_template.vehicle_request import VehicleRequest
+from modules.editly_template.editly_builder import EditlyBuilder
+from modules.editly_template.editly_runner import EditlyRunner
+
+from models.vehicleInfo import VehicleInfo, DealershipInfo
+import os
+# runner = EditlyRunner()
 
 
 class DummyService(Resource):
-    def get(self):
-        dummyModel = DummyModel("test name", "test year")
-        return {"name": dummyModel.name, "year": dummyModel.year}
+    def get(self, vin):
 
+        dummyModel = DummyModel("test name", "test year")
+        builder = EditlyBuilder()
+        vehicleRequest = VehicleRequest()
+        runner = EditlyRunner()
+        vehicleInfo = vehicleRequest.buildVehicleInfo(vin)
+        vehicleInfo.vehicle_local_imgs = vehicleRequest.downloadVehicleIMG(
+            vehicleInfo)
+        dataFile = builder.build(vehicleInfo)
+        runner.createAdaptiveRatioDataFile(dataFile, vehicleInfo)
+        runner.render()
+        return {"name": dummyModel.name, "year": dummyModel.year}
