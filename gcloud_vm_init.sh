cf22d70b5164bc59a6d7f2a0fe3730f756ef53be
truonghoangduy <truonghoangduy03111999@gmail.com>
2023-02-17 15:28:11 +0700

    add deps

--- gcloud_vm_init.sh
+++ gcloud_vm_init.sh
@@ -2,8 +2,6 @@ sudo apt-get update
 sudo apt-get install -y python
 sudo apt-get install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 
-sudo apt full-upgrade
-
 sudo apt-get install -y ffmpeg
 sudo apt-get install -y xvfb
 #
@@ -16,4 +14,6 @@ npm config set unsafe-perm true
 sudo npm i -g editly
 
 sudo apt-get install -y python3-pip
+#
+pip3 install -r requirements.txt
 # xvfb-run -s "-ac -screen 0 1280x1024x24" 
